410a3c9a2bda5883774a0cbb28ff7fb7b69d4f48
eunji <rkvpd1@naver.com>
2025-07-21 22:06:12 +0900

    feat:CORSadd

--- chat_server.py
+++ chat_server.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from langgraph_agent_react import run_agent
 
@@ -9,6 +10,14 @@ from langgraph_agent_react import run_agent
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Flutter 앱 주소를 명시해도 좋음 (보안상)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     query: str
